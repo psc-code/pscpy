3a5008c1fd28df43e4beaa38e3919064accda69b
James McClung <james@themcclungs.net>
2024-06-12 16:19:25 -0400

    pscpy: cleanup imports

--- src/pscpy/__init__.py
+++ src/pscpy/__init__.py
@@ -6,17 +6,8 @@ pscpy: Python utilities for reading PSC data
 
 from __future__ import annotations
 
+from pscpy import pscadios2
+
 from ._version import version as __version__
 
 __all__ = ["__version__"]
-
-
-import os
-from collections import namedtuple
-
-import numpy as np
-import xarray as xr
-
-import pscpy.adios2py
-
-from . import pscadios2
