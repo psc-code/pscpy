389fdce90de5bbfbf65fa0bd5e29c181271e4274
Kai Germaschewski <kai.germaschewski@gmail.com>
2024-12-19 22:53:01 -0500

    sample: add some sample data, and `sample_dir` to access it

--- src/pscpy/__init__.py
+++ src/pscpy/__init__.py
@@ -6,8 +6,13 @@ pscpy: Python utilities for reading PSC data
 
 from __future__ import annotations
 
+import pathlib
+
 from pscpy import pscadios2  # noqa: F401
 
 from ._version import version as __version__
+
+sample_dir = pathlib.Path(__file__).parent / "sample"
+
 
 __all__ = ["__version__"]
