f888880c4c10b0d96dbcd5f982c48ec86343add6
Kai Germaschewski <kai.germaschewski@gmail.com>
2024-12-21 09:26:43 -0500

    test_xarray_adios2: test some selections

--- tests/test_xarray_adios2.py
+++ tests/test_xarray_adios2.py
@@ -6,14 +6,37 @@ import xarray as xr
 import pscpy
 
 
-def test_open_dataset():
-    ds = xr.open_dataset(
+def _open_dataset() -> xr.Dataset:
+    return xr.open_dataset(
         pscpy.sample_dir / "pfd.000000400.bp",
         species_names=[],
         length=[1, 12.8, 51.2],
         corner=[0, -6.4, -25.6],
     )
+
+
+def test_open_dataset():
+    ds = _open_dataset()
     assert "jx_ec" in ds
     assert ds.coords.keys() == set({"x", "y", "z"})
     assert ds.jx_ec.shape == (1, 128, 512)
     assert np.allclose(ds.jx_ec.z, np.linspace(-25.6, 25.6, 512, endpoint=False))
+
+
+def test_component():
+    ds = _open_dataset()
+    assert ds.jeh.shape == (1, 128, 512, 9)
+    assert np.all(ds.jeh[..., 0] == ds.jx_ec)
+
+
+def test_selection():
+    ds = _open_dataset()
+    assert ds.jeh.shape == (1, 128, 512, 9)
+    assert np.all(ds.jeh[:, :10, :40, 0] == ds.jx_ec[:, :10, :40])
+
+
+def test_partial_selection():
+    ds = _open_dataset()
+    assert ds.jeh.shape == (1, 128, 512, 9)
+    assert np.all(ds.jeh[:, :10, ..., 0] == ds.jx_ec[:, :10, :])
+    assert np.all(ds.jeh[:, :10, ..., 0] == ds.jx_ec[:, :10])
