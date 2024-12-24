0dd361f15796816399376985ed61a4ead31270cd
Kai Germaschewski <kai.germaschewski@gmail.com>
2024-12-24 11:54:29 -0500

    adios2py: add test for repr()

--- tests/test_adios2py.py
+++ tests/test_adios2py.py
@@ -80,6 +80,19 @@ def test_variable_dtype(pfd_file):
         assert var.dtype == np.float32
 
 
+def test_variable_repr(pfd_file):
+    with pfd_file:
+        var = pfd_file.get_variable("jeh")
+        print(repr(var))
+        assert (
+            repr(var)
+            == "pscpy.adios2py.Variable(name=jeh, shape=(1, 128, 512, 9), dtype=float32"
+        )
+
+    with pytest.raises(ValueError, match="variable is closed"):
+        assert repr(var) == "jeh"
+
+
 def test_variable_is_reverse_dims(pfd_file):
     var = pfd_file.get_variable("jeh")
     assert not var.is_reverse_dims
