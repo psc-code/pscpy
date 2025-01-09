d76e8c981c6e8acd3ec6d07290e223e76d68380e
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-01-09 11:07:54 -0500

    test_Adios2Store: avoid ._state

--- tests/test_Adios2Store.py
+++ tests/test_Adios2Store.py
@@ -31,19 +31,19 @@ def test_open_close(test_store):
 
 
 def test_open_with_parameters(test_store):
-    filename = test_store.ds._state.filename
+    filename = test_store.ds.filename
     test_store.close()
 
     params = {"OpenTimeoutSecs": "20"}
     with Adios2Store.open(filename, parameters=params) as store:
-        assert store.ds._state.io.parameters() == params
+        assert store.ds.parameters == params
 
 
 def test_open_with_engine():
     with Adios2Store.open(
         str(pscpy.sample_dir / "pfd.000000400.bp"), engine_type="BP4"
     ) as store:
-        assert store.ds._state.io.engine_type() == "BP4"
+        assert store.ds.engine_type == "BP4"
 
 
 def test_vars_attrs(test_store):
