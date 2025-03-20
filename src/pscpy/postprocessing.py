35ae01163a6a5ca089e283749525f32cb97d2b88
James McClung <james@themcclungs.net>
2025-03-20 14:54:57 -0400

    postprocessing: pass test

--- src/pscpy/postprocessing.py
+++ src/pscpy/postprocessing.py
@@ -55,6 +55,10 @@ def auto_recenter(
         for dim, boundary_method in boundaries.items():
             if to_centering == "nc" and var_name.endswith(f"{dim}_ec"):
                 ds[var_name] = get_recentered(ds[var_name], dim, interp_dir, boundary=boundary_method)
+            elif to_centering == "cc" and var_name.endswith(f"{dim}_ec"):
+                for other_dim, other_boundary_method in boundaries.items():
+                    if other_dim != dim:
+                        ds[var_name] = get_recentered(ds[var_name], other_dim, interp_dir, boundary=other_boundary_method)
 
         new_name = var_name[:-3] + "_" + to_centering
         ds[new_name] = ds[var_name].rename(new_name)
