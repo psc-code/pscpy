de8fca912e9d0e24fd88950044255ac25603470b
James McClung <james@themcclungs.net>
2025-03-20 12:16:50 -0400

    +test_postprocessing: +recenter tests

---
+++ tests/test_postprocessing.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import numpy as np
+import pytest
+import xarray as xr
+
+import pscpy
+
+
+@pytest.fixture
+def test_dataarray():
+    return xr.DataArray([4, 5, 6, 5], coords={"x": [0, 1, 2, 3]})
+
+
+def test_recenter_periodic_left(test_dataarray):
+    recentered = pscpy.recenter(test_dataarray, "x", -1)
+    assert np.array_equal(recentered.coords, test_dataarray.coords)
+    assert np.array_equal(recentered, [4.5, 4.5, 5.5, 5.5])
+
+
+def test_recenter_periodic_right(test_dataarray):
+    recentered = pscpy.recenter(test_dataarray, "x", 1)
+    assert np.array_equal(recentered.coords, test_dataarray.coords)
+    assert np.array_equal(recentered, [4.5, 5.5, 5.5, 4.5])
+
+
+def test_recenter_pad_left(test_dataarray):
+    recentered = pscpy.recenter(test_dataarray, "x", -1, boundary="pad")
+    assert np.array_equal(recentered.coords, test_dataarray.coords)
+    assert np.array_equal(recentered, [4, 4.5, 5.5, 5.5])
+
+
+def test_recenter_pad_right(test_dataarray):
+    recentered = pscpy.recenter(test_dataarray, "x", 1, boundary="pad")
+    assert np.array_equal(recentered.coords, test_dataarray.coords)
+    assert np.array_equal(recentered, [4.5, 5.5, 5.5, 5])
+
+
+def test_recenter_zero_left(test_dataarray):
+    recentered = pscpy.recenter(test_dataarray, "x", -1, boundary="zero")
+    assert np.array_equal(recentered.coords, test_dataarray.coords)
+    assert np.array_equal(recentered, [2, 4.5, 5.5, 5.5])
+
+
+def test_recenter_zero_right(test_dataarray):
+    recentered = pscpy.recenter(test_dataarray, "x", 1, boundary="zero")
+    assert np.array_equal(recentered.coords, test_dataarray.coords)
+    assert np.array_equal(recentered, [4.5, 5.5, 5.5, 2.5])
