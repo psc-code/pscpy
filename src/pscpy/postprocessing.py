772daf98dac9479343639c090a29d6b4b34df5a8
James McClung <james@themcclungs.net>
2025-03-20 12:17:04 -0400

    postprocessing: implement recenter

--- src/pscpy/postprocessing.py
+++ src/pscpy/postprocessing.py
@@ -18,4 +18,14 @@ def recenter(
     For example, `interp_dir=-1` interpolates node-centered values from cell-centered values, because each node center is at a lesser coordinate than the cell center of the same index.
     """
 
-    return da
+    shifted = da.roll({dim: -interp_dir}, roll_coords=False)
+    boundary_idx = {-1: 0, 1: -1}[interp_dir]
+
+    if boundary == "periodic":
+        pass  # this case is already handled by the behavior of roll()
+    elif boundary == "pad":
+        shifted[{dim: boundary_idx}] = da[{dim: boundary_idx}]
+    elif boundary == "zero":
+        shifted[{dim: boundary_idx}] = 0
+
+    return 0.5 * (da + shifted)
