b2a4c5e303bb25e71049a88cddc434f20ee2cb30
James McClung <james@themcclungs.net>
2025-03-20 14:45:24 -0400

    test_postprocessing: +test_autorecenter_ec_to_nc

--- tests/test_postprocessing.py
+++ tests/test_postprocessing.py
@@ -46,3 +46,18 @@ def test_recenter_zero_right(test_dataarray):
     recentered = pscpy.get_recentered(test_dataarray, "x", 1, boundary="zero")
     assert np.array_equal(recentered.coords, test_dataarray.coords)
     assert np.array_equal(recentered, [4.5, 5.5, 5.5, 2.5])
+
+
+@pytest.fixture
+def test_dataset_ec():
+    coords = [[0, 1], [0, 1, 2]]
+    dims = ["x", "y"]
+    ex_ec = xr.DataArray([[0, 1, 2], [3, 4, 5]], coords, dims)
+    ey_ec = xr.DataArray([[0, 2, 4], [1, 3, 5]], coords, dims)
+    return xr.Dataset({"ex_ec": ex_ec, "ey_ec": ey_ec})
+
+
+def test_autorecenter_ec_to_nc(test_dataset_ec):
+    pscpy.auto_recenter(test_dataset_ec, "nc", x="pad", y="pad")
+    assert np.array_equal(test_dataset_ec.ex_nc, [[0, 1, 2], [1.5, 2.5, 3.5]])
+    assert np.array_equal(test_dataset_ec.ey_nc, [[0, 1, 3], [1, 2, 4]])
